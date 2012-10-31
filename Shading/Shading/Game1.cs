10fcf973235c13c07c35bfdfae4c15923f4b2f52
Brian Schwind <brian.schwind@furman.edu>
2012-10-30 23:45:13 -0400

    Cleaned up the now unnecessary pieces of code

--- Shading/Shading/Game1.cs
+++ Shading/Shading/Game1.cs
@@ -16,15 +16,10 @@ namespace Shading
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Model cathedral;
-        Texture2D tex;
         Camera cam;
-        Effect gaussianEffect;
-
-        RenderTarget2D normalTarget;
-        RenderTarget2D tempTarget1, tempTarget2;
-        float sig = 12;
-
+        float sigma = 12;
         Renderer renderer;
+        GaussianBlurPP gaussian;
 
         public Game1()
         {
@@ -47,75 +42,11 @@ namespace Shading
             cam = new Camera(2.5f, 0.01f, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
 
             cathedral = Content.Load<Model>("Models/dragon");
-            tex = Content.Load<Texture2D>("Models/lol");
-            Effect normalEffect = Content.Load<Effect>("Effects/RenderNormals");
-            gaussianEffect = Content.Load<Effect>("Effects/GaussianBlur");
-            setupGaussianBlur(sig);
-            replaceModelEffect(cathedral, normalEffect);
-
-            normalTarget = new RenderTarget2D(GraphicsDevice,
-                                              GraphicsDevice.Viewport.Width,
-                                              GraphicsDevice.Viewport.Height,
-                                              false,
-                                              SurfaceFormat.Color,
-                                              DepthFormat.Depth16);
-
-            tempTarget1 = new RenderTarget2D(GraphicsDevice,
-                                              GraphicsDevice.Viewport.Width,
-                                              GraphicsDevice.Viewport.Height,
-                                              false,
-                                              SurfaceFormat.Color,
-                                              DepthFormat.None);
-
-            tempTarget2 = new RenderTarget2D(GraphicsDevice,
-                                              GraphicsDevice.Viewport.Width,
-                                              GraphicsDevice.Viewport.Height,
-                                              false,
-                                              SurfaceFormat.Color,
-                                              DepthFormat.None);
 
             renderer = new Renderer(Content, GraphicsDevice, spriteBatch);
             renderer.AddModel(cathedral);
-        }
-
-        private void setupGaussianBlur(float sigma)
-        {
-            // Look up the sample weight effect parameters.
-            EffectParameter weightsParameter;
-            weightsParameter = gaussianEffect.Parameters["SampleWeights"];
-
-            int samples = weightsParameter.Elements.Count;
-
-            //Due to symmetry, we only store the sample weights for one side of
-            //the middle pixel
-            float[] sampleWeights = new float[samples];
-            float totalWeight = 0f;
-
-            for (int i = 0; i < samples; i++)
-            {
-                //We take samples halfway between two pixels to take advantage of
-                //bilinear filtering
-                sampleWeights[i] = computeGaussian(i * 2 + 1.5f, sigma);
-
-                if (i > 0)
-                {
-                    totalWeight += sampleWeights[i] * 2;
-                }
-                else
-                {
-                    totalWeight += sampleWeights[i];
-                }
-            }
-
-            //Normalize the weights so they sum to 1
-            for (int i = 0; i < samples; i++)
-            {
-                sampleWeights[i] /= totalWeight;
-            }
-
-            weightsParameter.SetValue(sampleWeights);
-            gaussianEffect.Parameters["dx"].SetValue(1f / GraphicsDevice.Viewport.Width);
-            gaussianEffect.Parameters["dy"].SetValue(1f / GraphicsDevice.Viewport.Height);
+            gaussian = new GaussianBlurPP(renderer.PostProcessor, sigma);
+            renderer.PostProcessor.AddPPEffect(gaussian);
         }
 
         private void replaceModelEffect(Model model, Effect effect)
@@ -145,11 +76,11 @@ namespace Shading
 
             GamePadState padState = GamePad.GetState(PlayerIndex.One);
 
-            sig += padState.Triggers.Right;
-            sig -= padState.Triggers.Left;
-            sig = MathHelper.Clamp(sig, 0.5f, 500);
+            sigma += padState.Triggers.Right;
+            sigma -= padState.Triggers.Left;
+            sigma = MathHelper.Clamp(sigma, 0.5f, 100);
 
-            setupGaussianBlur(sig);
+            gaussian.Sigma = sigma;
 
             base.Update(gameTime);
         }
@@ -158,57 +89,7 @@ namespace Shading
         {
             renderer.Render(gameTime, cam);
 
-            /*GraphicsDevice.SetRenderTarget(normalTarget);
-            GraphicsDevice.Clear(Color.Transparent);
-
-            float elapsed = (float)gameTime.TotalGameTime.TotalSeconds;
-
-            foreach (ModelMesh mesh in cathedral.Meshes)
-            {
-                foreach (ModelMeshPart meshPart in mesh.MeshParts)
-                {
-                    Effect effect = meshPart.Effect;
-
-                    effect.Parameters["World"].SetValue(Matrix.Identity);
-                    effect.Parameters["View"].SetValue(cam.View);
-                    effect.Parameters["Projection"].SetValue(cam.Projection);
-                }
-                
-                mesh.Draw();
-            }
-
-            //Blur horizontally
-            GraphicsDevice.SetRenderTarget(tempTarget1);
-            gaussianEffect.CurrentTechnique = gaussianEffect.Techniques["HorizontalGaussianBlur"];
-            spriteBatch.Begin(0, BlendState.Opaque, null, null, null, gaussianEffect);
-            spriteBatch.Draw(normalTarget, Vector2.Zero, Color.White);
-            spriteBatch.End();
-
-            //Blur vertically
-            GraphicsDevice.SetRenderTarget(tempTarget2);
-            gaussianEffect.CurrentTechnique = gaussianEffect.Techniques["VerticalGaussianBlur"];
-            spriteBatch.Begin(0, BlendState.Opaque, null, null, null, gaussianEffect);
-            spriteBatch.Draw(tempTarget1, Vector2.Zero, Color.White);
-            spriteBatch.End();
-
-            //Draw the composited blur
-            GraphicsDevice.SetRenderTarget(null);
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            spriteBatch.Begin();
-            spriteBatch.Draw(tempTarget2, Vector2.Zero, Color.White);
-            spriteBatch.End();
-
-            GraphicsDevice.BlendState = BlendState.Opaque;
-            GraphicsDevice.DepthStencilState = DepthStencilState.Default;*/
-
             base.Draw(gameTime);
-        }
-
-        private static float computeGaussian(float n, float sigma)
-        {
-            return (float)((1.0 / Math.Sqrt(2 * Math.PI * sigma * sigma)) *
-                            Math.Exp(-(n * n) / (2 * sigma * sigma)));
         }
     }
 }
